aae7ff4e9460bb1fb0e4f53cc0dbeb9333751f2b
Tharegon <dombovari11david@gmaa>
2021-04-16 12:07:00 +0200

    Add basic Main

--- src/main/java/com/codecool/App.java
+++ src/main/java/com/codecool/App.java
@@ -1,13 +1,12 @@
 package com.codecool;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        View view = new View();
+        Controller controller = new Controller(view);
+        controller.menu();
     }
 }
