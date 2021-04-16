04ac4b0aa59865cf3e35fd7504f7c857d7f451a9
Tharegon <dombovari11david@gmaa>
2021-04-16 12:06:45 +0200

    Add basic View

---
+++ src/main/java/com/codecool/View.java
@@ -0,0 +1,18 @@
+package com.codecool;
+
+public class View {
+
+    public void menuTitle() {
+        System.out.println("This is the menu");
+    }
+
+    public void linePrinter(String line){
+        System.out.println(line);
+    }
+
+    public void showReports(){
+        System.out.println("1.option");
+        System.out.println("2.option");
+        System.out.println("3.option");
+    }
+}
