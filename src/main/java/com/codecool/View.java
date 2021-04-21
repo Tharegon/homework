68e6400a2532a3cb9f6d2f5fc34f1ea2aedb80ff
Tharegon <dombovari11david@gmaa>
2021-04-21 17:03:00 +0200

    Add printList

--- src/main/java/com/codecool/View.java
+++ src/main/java/com/codecool/View.java
@@ -1,5 +1,9 @@
 package com.codecool;
 
+import com.codecool.entity.PlantOrder;
+
+import java.util.List;
+
 public class View {
 
     public void menuTitle() {
@@ -14,5 +18,12 @@ public class View {
         System.out.println("1.option");
         System.out.println("2.option");
         System.out.println("3.option");
+    }
+
+    public void printList(List<PlantOrder> POJO) {
+        for (PlantOrder order: POJO
+             ) {
+            System.out.println(order);
+        }
     }
 }
