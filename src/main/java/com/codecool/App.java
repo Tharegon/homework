c7bcc9d3bc4e549393ec9f100ec0887f01fc6b3b
Tharegon <dombovari11david@gmaa>
2021-04-21 17:03:11 +0200

    Add service

--- src/main/java/com/codecool/App.java
+++ src/main/java/com/codecool/App.java
@@ -6,7 +6,8 @@ public class App
     public static void main( String[] args )
     {
         View view = new View();
-        Controller controller = new Controller(view);
+        RestAPIService service = new RestAPIService();
+        Controller controller = new Controller(view,service);
         controller.menu();
     }
 }
