14b466d87724ebaa4d57e5ff78c1db7336400f7e
Tharegon <dombovari11david@gmaa>
2021-04-24 11:55:12 +0200

    Add LocationDao

---
+++ src/main/java/com/codecool/dao/LocationDao.java
@@ -0,0 +1,34 @@
+package com.codecool.dao;
+
+import com.codecool.HibernateUtil;
+import com.codecool.entity.Location;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+
+public class LocationDao {
+    public void saveLocation(Location location) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            session.save(location);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public List< Location > getLocation() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Location ", Location.class).list();
+        }
+    }
+}
