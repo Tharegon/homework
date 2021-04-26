7ab903de583f2ac466a7b0d697a2f5f6ecf4571c
Tharegon <dombovari11david@gmaa>
2021-04-26 14:42:09 +0200

    Add updated http request

--- src/main/java/com/codecool/RestAPIService.java
+++ src/main/java/com/codecool/RestAPIService.java
@@ -3,7 +3,9 @@ package com.codecool;
 
 
 import com.codecool.entity.Location;
+import com.codecool.entity.Marketplace;
 import com.codecool.entity.PlantOrder;
+import com.codecool.entity.Status;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -19,6 +21,7 @@ import org.apache.hc.client5.http.impl.classic.HttpClients;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -27,18 +30,26 @@ public class RestAPIService {
 
     public List<PlantOrder> generatePlantOrderPOJO() throws Exception{
 
+            ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-            /*HttpGet request = new HttpGet("https://my.api.mockaroo.com/listing?key=63304c70");
+            /*
+            HttpGet request = new HttpGet("https://my.api.mockaroo.com/listing?key=63304c70");
 
-            PlantOrder response = client.execute(request, httpResponse ->
-                    mapper.readValue(httpResponse.getEntity().getContent().toString(), PlantOrder.class));
-            */
-            ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            CloseableHttpClient client=  HttpClients.createDefault();
+            PlantOrder[] response = client.execute(request, httpResponse ->{
+                System.out.println(httpResponse.getCode());
+                String body = httpResponse.getEntity().getContent().toString();
+                System.out.println(body);
+                return mapper.readValue(body, PlantOrder[].class);
+                return Arrays.asList(response);
+            });*/
+
+
 
             InputStream input = new FileInputStream("src/main/java/com/codecool/json/order.json");
 
-            //PlantOrder plantOrder = objectMapper.readValue(input, PlantOrder.class);
-            List<PlantOrder> plantOrder = objectMapper.readValue(input, new TypeReference<List<PlantOrder>>() {});
+
+            List<PlantOrder> plantOrder = mapper.readValue(input, new TypeReference<List<PlantOrder>>() {});
             return plantOrder;
     }
 
@@ -47,6 +58,20 @@ public class RestAPIService {
         InputStream input = new FileInputStream("src/main/java/com/codecool/json/location.json");
         List<Location> locations = objectMapper.readValue(input, new TypeReference<List<Location>>() {});
         return locations;
+    }
+
+    public List<Status> generateStatusPOJO() throws Exception{
+        ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        InputStream input = new FileInputStream("src/main/java/com/codecool/json/status.json");
+        List<Status> statuses = objectMapper.readValue(input, new TypeReference<List<Status>>() {});
+        return statuses;
+    }
+
+    public List<Marketplace> generateMarketplacePOJO() throws Exception{
+        ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        InputStream input = new FileInputStream("src/main/java/com/codecool/json/marketplace.json");
+        List<Marketplace> marketplaces = objectMapper.readValue(input, new TypeReference<List<Marketplace>>() {});
+        return marketplaces;
     }
     }
 
