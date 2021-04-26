6520d7f71274a8a57b74eaa837906e66bcc70da1
Tharegon <dombovari11david@gmaa>
2021-04-26 14:43:03 +0200

    Add Marketplace, MarketplaceDao and json

---
+++ src/main/java/com/codecool/dao/MarketplaceDao.java
@@ -0,0 +1,37 @@
+package com.codecool.dao;
+
+import com.codecool.HibernateUtil;
+import com.codecool.entity.Marketplace;
+import com.codecool.entity.PlantOrder;
+import com.codecool.entity.Status;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class MarketplaceDao {
+
+    public void saveMarketplace(Marketplace marketplace) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            session.save(marketplace);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public List<Marketplace> getMarketplaces() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Marketplace ", Marketplace.class).list();
+        }
+    }
+}
+
---
+++ src/main/java/com/codecool/entity/Marketplace.java
@@ -0,0 +1,31 @@
+package com.codecool.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table
+@Entity
+public class Marketplace {
+
+    @Id
+    private Integer marketplaceId;
+    private String marketplaceName;
+
+    public Marketplace(@JsonProperty("id") Integer marketplaceId,
+                       @JsonProperty("marketplace_name") String marketplaceName) {
+        this.marketplaceId = marketplaceId;
+        this.marketplaceName = marketplaceName;
+    }
+}
