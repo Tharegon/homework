240f512c654311946fa4d1ad2eb46ee58ffb05ff
Tharegon <dombovari11david@gmaa>
2021-04-26 23:00:02 +0200

    Add database generation

--- src/main/java/com/codecool/Controller.java
+++ src/main/java/com/codecool/Controller.java
@@ -1,14 +1,19 @@
 package com.codecool;
 
 import com.codecool.dao.LocationDao;
+import com.codecool.dao.MarketplaceDao;
 import com.codecool.dao.PlantOrderDao;
+import com.codecool.dao.StatusDao;
 import com.codecool.entity.Location;
+import com.codecool.entity.Marketplace;
 import com.codecool.entity.PlantOrder;
+import com.codecool.entity.Status;
+import com.codecool.pojo.LocationPojo;
+import com.codecool.pojo.PlantOrderPojo;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.UUID;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.*;
 
 public class Controller {
 
@@ -22,12 +27,12 @@ public class Controller {
         this.service = service;
     }
 
-    private String readLine(){
+    private String readLine() {
         Scanner myScanner = new Scanner(System.in);
         return myScanner.nextLine();
     }
 
-    public void menu(){
+    public void menu() {
         view.menuTitle();
         try {
             readCommand(readLine());
@@ -37,20 +42,76 @@ public class Controller {
 
     }
 
-    private void readCommand(String command) throws Exception{
-        switch (command){
+    private void readCommand(String command) throws Exception {
+        switch (command) {
             case "report":
                 view.showReports();
                 break;
             case "database":
-                List<PlantOrder> orders = service.generatePlantOrderPOJO();
-                List<Location> locations = service.generateLocationPOJO();
+                List<PlantOrderPojo> orders = service.generatePlantOrderPOJO();
+                List<LocationPojo> locations = service.generateLocationPOJO();
+                List<Status> statuses = service.generateStatusPOJO();
+                List<Marketplace> marketplaces = service.generateMarketplacePOJO();
+                MarketplaceDao marketplaceDao = new MarketplaceDao();
+                StatusDao statusDao = new StatusDao();
 
+                marketplaces.forEach(marketplaceDao::saveMarketplace);
+                statuses.forEach(statusDao::saveStatus);
+
+
+                HashMap<UUID, LocationPojo> locationsMap = new HashMap<>();
+
+                for (LocationPojo location : locations) {
+                    locationsMap.put(location.getLocationId(), location);
+                }
+                List<PlantOrder> orderList = new ArrayList<>();
                 PlantOrderDao plantOrderDao = new PlantOrderDao();
+                LocationDao locationDao = new LocationDao();
+                Set<Location> locationsSet=new HashSet<>();
+
+                String empty = null;
+                for (PlantOrderPojo orderPojo : orders) {
+                    if (locationsMap.containsKey(orderPojo.getInventoryItemLocationId())) {
+                        LocationPojo locationPojo = locationsMap.getOrDefault(orderPojo.getInventoryItemLocationId(), null);
+
+                        Location location = new Location(locationPojo.getLocationId().toString(),
+                                locationPojo.getManagerName(),
+                                locationPojo.getPhone(),
+                                locationPojo.getAddressPrimary(),
+                                locationPojo.getAddressSecondary(),
+                                locationPojo.getCountry(),
+                                locationPojo.getTown(),
+                                locationPojo.getPostalCode());
+
+                        locationsSet.add(location);
+
+                        PlantOrder order = new PlantOrder(orderPojo.getOrderId().toString(),
+                                orderPojo.getTitle(),
+                                orderPojo.getDescription(),
+                                location,
+                                orderPojo.getListingPrice(),
+                                orderPojo.getCurrency(),
+                                orderPojo.getQuantity(),
+                                StatusDao.getStatusById(orderPojo.getListingStatus()),
+                                marketplaceDao.getMarketplaceById(orderPojo.getMarketplace()),
+
+                                null != orderPojo.getUploadTime() ? new SimpleDateFormat("dd/MM/yyyy").parse(orderPojo.getUploadTime()) : Date.from(Instant.now()),
+                                orderPojo.getOwnerEmailAddress());
+                        orderList.add(order);
+                    }
+                }
+                locationsSet.forEach(locationDao::saveLocation);
+                orderList.forEach(plantOrderDao::saveOrder);
+
+
+                /*PlantOrderDao plantOrderDao = new PlantOrderDao();
                 LocationDao locationDao = new LocationDao();
 
                 orders.forEach(s -> plantOrderDao.saveOrder(s));
                 locations.forEach(x -> locationDao.saveLocation(x));
+                marketplaces.forEach(c->marketplaceDao.saveMarketplace(c));
+                statuses.forEach(y->statusDao.saveStatus(y));*/
+
 
                 readCommand(readLine());
             case "exit":
